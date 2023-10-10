8bddad4590056bf5f361a155e30394fd70242763
Saadi-Yip <saadshabbir@leilanitech.com>
2023-10-10 10:14:44 +0500

    updating

--- routes/routes.js
+++ routes/routes.js
@@ -11,6 +11,6 @@ const { update_user } = require("../controllers/dashboard/UserController");
 router.post("/login", postLogin);  /*** Login User ***/
 router.route("/user/:id")
   .patch(upload.single("profile"), update_user)  /*** Update Existing User ***/
-router.get("/", (req, res) => { res.send("<h1>Your Internet Provider</h1>") });  /*** Main Route ***/
+router.get("/", (req, res) => { res.send("<h1>Dashboard</h1>") });  /*** Main Route ***/
 // -- /*** Export all Routes ***/ -- // 
 module.exports = router;
